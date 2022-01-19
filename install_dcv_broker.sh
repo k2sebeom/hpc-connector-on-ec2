28d07076f9ee8383b04082489bc380b871023a5a
k2sebeom <moses97@gmail.com>
2022-01-20 05:58:14 +0900

    Change ports

--- install_dcv_broker.sh
+++ install_dcv_broker.sh
@@ -1,6 +1,10 @@
 rpm --import https://d1uj6qtbmh3dt5.cloudfront.net/NICE-GPG-KEY
 wget https://d1uj6qtbmh3dt5.cloudfront.net/2021.3/SessionManagerBrokers/nice-dcv-session-manager-broker-2021.3.307-1.el7.noarch.rpm
 yum install -y nice-dcv-session-manager-broker-2021.3.307-1.el7.noarch.rpm
+
+sed -i 's/client-to-broker-connector-https-port = .*$/client-to-broker-connector-https-port = 8446/' \
+    /etc/dcv-session-manager-broker/session-manager-broker.properties
+
 systemctl enable dcv-session-manager-broker
 systemctl start dcv-session-manager-broker
 
@@ -8,9 +12,9 @@ echo "==================="
 echo "Installed DCV Session Manager Broker"
 echo "==================="
 
-sleep 3
+sleep 10
 
-cp /var/lib/dcvsmbroker/security/dcvsmbroker_ca.pem .
+cp /var/lib/dcvsmbroker/security/dcvsmbroker_ca.pem /home/efadmin/.
 
 java_path=$(readlink /etc/alternatives/java | sed 's/\/bin\/java//')
 openssl x509 -in /var/lib/dcvsmbroker/security/dcvsmbroker_ca.pem -inform pem \
