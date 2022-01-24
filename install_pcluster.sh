1c5dc434b464a7761e6372dbd3ea24c168ebdb13
k2sebeom <moses97@gmail.com>
2022-01-24 18:47:19 +0900

    feat: Fix install pcluster script

--- install_pcluster.sh
+++ install_pcluster.sh
@@ -1,6 +1,6 @@
 cd /home/efnobody
 python3 -m venv .venv
-echo "source .venv/bin/activate" >> ~/.bashrc
+echo "source /home/efnobody/.venv/bin/activate" >> ~/.bashrc
 source .venv/bin/activate
 pip3 install --upgrade pip
 pip3 install aws-parallelcluster
--- master.sh
+++ master.sh
@@ -1,4 +1,4 @@
 ./install_dcv_broker.sh
 ./parse_config.sh
-./set_pcluster.sh
+./install_pcluster.sh
 ./install_enginframe.sh
